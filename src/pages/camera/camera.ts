7232192667c600b71b1d9f1b7f2ceaf53ed07590
WROBLEWSKI <wrobpierre@hotmail.fr>
2018-02-26 09:25:47 +0100

    Adding the 5 photo per student

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CameraPreview, CameraPreviewOptions , CameraPreviewPictureOptions } from '@ionic-native/camera-preview';
 import { DataProvider } from '../../providers/data-provider/data-provider';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
 /**
 * Generated class for the CameraPage page.
@@ -14,20 +15,28 @@ import { DataProvider } from '../../providers/data-provider/data-provider';
 @Component({
     selector: 'page-camera',
     templateUrl: 'camera.html',
+    providers: [
+        ScreenOrientation
+    ]
 })
 export class CameraPage {
-    public picture: string;
-    public picturePreview: string;
+    public picture = [];
+    public nbPicture = 5;
+    public picturePreview = [];
+    public isHide : boolean;
+    public position = 0;
 
     public pictureOpts: CameraPreviewPictureOptions;
     public student: any;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private cameraPreview: CameraPreview, private dataProvider: DataProvider) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private cameraPreview: CameraPreview, private dataProvider: DataProvider, private screenOrientation: ScreenOrientation) {
         this.student = navParams.get("student");
-        this.startCamera();
+        this.isHide = true;
+        this.nbPicture = 0;
+        this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
     }
 
-    startCamera() {
+    startCamera(position) {
         const cameraPreviewOpts: CameraPreviewOptions = {
             x: 0,
             y: (window.screen.height/100)*10,
@@ -42,6 +51,8 @@ export class CameraPage {
 
         this.cameraPreview.startCamera(cameraPreviewOpts);
         this.cameraPreview.show();
+        this.isHide = false;
+        this.position = position;
     };
 
     takePicture() {
@@ -50,33 +61,43 @@ export class CameraPage {
             height: 1280,
             quality: 85
         }
-        // take a picture
         this.cameraPreview.takePicture(this.pictureOpts).then((imageData) =>
         {
-            this.picture = imageData;
-            this.picturePreview = "data:image/png;base64," + imageData;
+            this.picture[this.position] = imageData;
+            this.picturePreview[this.position] = "data:image/png;base64," + imageData;
         }, (err) => {
-            this.picture = 'assets/img/test.jpg';
+            this.picture[this.position] = 'assets/img/test.jpg';
         });
 
         this.cameraPreview.hide();
+        this.isHide = true;
     }
 
     showCamera() {
         this.cameraPreview.show();
+        this.isHide = false;
     }
 
     pushPicture() {
-        this.student.pictures.push(this.picture);
+        for(var i = 1; i <= this.nbPicture; i++){
+            if(this.picture[i] != null){
+                this.student.pictures.push(this.picture[i]);
+            }
+        }
         this.dataProvider.updateStudent(this.student);
 
         this.picture = null;
         this.picturePreview = null;
-        this.showCamera();
+    }
+
+    hideCamera(){
+        this.isHide = true;
+        this.cameraPreview.hide();
     }
 
     goBack(){
+        this.isHide = true;
+        this.cameraPreview.stopCamera();
         this.navCtrl.pop();
-        this.cameraPreview.hide();
     }
 }
