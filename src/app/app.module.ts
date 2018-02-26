4951d0256c362023ead893a29ab49a2ce8f1bae2
Sullivan <sullivanroirand@protonmail.com>
2018-02-26 14:59:10 +0100

    Add pin code for admin login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,24 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule } from '@ionic/storage';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Camera } from '@ionic-native/camera';
 import { EditStudentPage } from '../pages/edit-student/edit-student'
 import { AddStudentPage } from '../pages/add-student/add-student'
-
 import { PdfPage } from '../pages/pdf/pdf'
 import { PopoverPage } from '../pages/popover/popover'
 import { CameraPage } from '../pages/camera/camera'
 import { CameraPreview } from '@ionic-native/camera-preview';
 import { DataProvider } from '../providers/data-provider/data-provider';
-
 import { File } from '@ionic-native/file';
 import { FileOpener } from '@ionic-native/file-opener';
 import { EmailComposer } from '@ionic-native/email-composer';
 import { Base64 } from '@ionic-native/base64';
 import { AuthentificationProvider } from '../providers/authentification/authentification';
 import { StudentModalPage } from '../pages/student-modal/student-modal';
+import { PincodeInputModule } from  'ionic2-pincode-input';
 
 @NgModule({
     declarations: [
@@ -43,6 +41,8 @@ import { StudentModalPage } from '../pages/student-modal/student-modal';
             driverOrder: ['indexeddb', 'sqlite', 'websql']
         }),
         BrowserModule,
+        PincodeInputModule,
+        BrowserAnimationsModule,
         IonicModule.forRoot(MyApp)
     ],
     bootstrap: [IonicApp],
--- src/pages/popover/popover.ts
+++ src/pages/popover/popover.ts
@@ -2,13 +2,14 @@ import { NavController, ViewController } from "ionic-angular";
 import { Component } from "@angular/core";
 import { AuthentificationProvider } from '../../providers/authentification/authentification';
 import { PdfPage } from "../pdf/pdf";
+import { PincodeController } from  'ionic2-pincode-input/dist/pincode'
 
 @Component({
     selector: 'popover',
     templateUrl: 'popover.html',
 })
 export class PopoverPage {
-    constructor(public viewCtrl: ViewController, public navCtrl: NavController, public authentificationProvider: AuthentificationProvider) {
+    constructor(public pincodeCtrl: PincodeController, public viewCtrl: ViewController, public navCtrl: NavController, public authentificationProvider: AuthentificationProvider) {
 
     }
 
@@ -16,7 +17,29 @@ export class PopoverPage {
         this.viewCtrl.dismiss();
     }
 
-    admin(){
-        this.authentificationProvider.is_admin = !this.authentificationProvider.is_admin;
+    admin():any{
+        if(!this.authentificationProvider.is_admin)
+        {
+            let pinCode =  this.pincodeCtrl.create({
+                title: 'Code PIN',
+                hideForgotPassword: true,
+                hideCancelButton: true,
+                passSize: 4
+            });
+
+            pinCode.present();
+
+            pinCode.onDidDismiss( (code,status) => {
+                if(status === 'done'){
+                    if(code == this.authentificationProvider.admin_pin) {
+                        this.authentificationProvider.is_admin = true;
+                    }
+                }
+            })
+        }
+        else
+        {
+            this.authentificationProvider.is_admin = false;
+        }
     }
 }
